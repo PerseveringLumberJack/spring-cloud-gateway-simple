1979f6fc7cd9b22b48d7eff7b8bbb57e574ea707
PerseveringLumberJack <873298352@qq.com>
2021-08-27 11:13:32 +0800

    pom

--- src/test/java/com/example/springcloud/gatewaysimple/SpringCloudGatewaySimpleApplicationTests.java
+++ src/test/java/com/example/springcloud/gatewaysimple/SpringCloudGatewaySimpleApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.springcloud.gatewaysimple;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCloudGatewaySimpleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
