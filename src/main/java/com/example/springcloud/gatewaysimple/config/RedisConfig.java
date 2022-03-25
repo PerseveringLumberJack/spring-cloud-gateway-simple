13ca257963bafb5dc46c345a9ebeb4967fd43ac5
zhangyongqiang <zhangyongqiang@leomaster.com>
2022-03-25 11:32:24 +0800

    @

---
+++ src/main/java/com/example/springcloud/gatewaysimple/config/RedisConfig.java
@@ -0,0 +1,171 @@
+package com.example.springcloud.gatewaysimple.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring")
+@Getter
+@Setter
+public class RedisConfig {
+
+    private RedisProperties redis;
+
+    @Bean(destroyMethod = "shutdown")
+    @ConditionalOnMissingBean(ClientResources.class)
+    public DefaultClientResources lettuceClientResources() {
+        return DefaultClientResources.create();
+    }
+
+    @Bean(name = "redisTemplate")
+    @Primary
+    public RedisTemplate RedisTemplate(
+            @Qualifier("firstRedisLettuceConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // 设置value的序列化规则和 key的序列化规则
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+
+    @Bean(name = "firstRedisLettuceConnectionFactory")
+    @Primary
+    public LettuceConnectionFactory Redis1LettuceConnectionFactory(ClientResources clientResources) {
+        RedisStandaloneConfiguration redis1StandaloneConfiguration = getStandaloneConfig(redis);
+        LettuceClientConfiguration clientConfig = getLettuceClientConfiguration(clientResources, redis);
+        return new LettuceConnectionFactory(redis1StandaloneConfiguration, clientConfig);
+    }
+
+
+    /**
+     * redis standalone config
+     *
+     * @param redisProperties redis 配置参数
+     * @return RedisStandaloneConfiguration
+     */
+    private RedisStandaloneConfiguration getStandaloneConfig(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.getHost());
+        config.setPort(redisProperties.getPort());
+        config.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        config.setDatabase(redisProperties.getDatabase());
+        return config;
+    }
+
+    /**
+     * 构建 LettuceClientConfiguration
+     *
+     * @param clientResources clientResources
+     * @param redisProperties redisProperties
+     * @return LettuceClientConfiguration
+     */
+    private LettuceClientConfiguration getLettuceClientConfiguration(ClientResources clientResources,
+                                                                     RedisProperties redisProperties) {
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder =
+                createBuilder(redisProperties.getLettuce().getPool());
+        if (redisProperties.isSsl()) {
+            builder.useSsl();
+        }
+        if (redisProperties.getTimeout() != null) {
+            builder.commandTimeout(redisProperties.getTimeout());
+        }
+        if (redisProperties.getLettuce() != null) {
+            RedisProperties.Lettuce lettuce = redisProperties.getLettuce();
+            if (lettuce.getShutdownTimeout() != null
+                    && !lettuce.getShutdownTimeout().isZero()) {
+                builder.shutdownTimeout(
+                        redisProperties.getLettuce().getShutdownTimeout());
+            }
+        }
+        builder.clientResources(clientResources);
+        return builder.build();
+    }
+
+    /**
+     * 创建 LettuceClientConfigurationBuilder
+     *
+     * @param pool 连接池配置
+     * @return LettuceClientConfigurationBuilder
+     */
+    private LettuceClientConfiguration.LettuceClientConfigurationBuilder createBuilder(RedisProperties.Pool pool) {
+        if (pool == null) {
+            return LettuceClientConfiguration.builder();
+        }
+        return LettucePoolingClientConfiguration.builder().poolConfig(getPoolConfig(pool));
+    }
+
+    /**
+     * pool config
+     *
+     * @param properties redis 参数配置
+     * @return GenericObjectPoolConfig
+     */
+    private GenericObjectPoolConfig getPoolConfig(RedisProperties.Pool properties) {
+        GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+        config.setMaxTotal(properties.getMaxActive());
+        config.setMaxIdle(properties.getMaxIdle());
+        config.setMinIdle(properties.getMinIdle());
+        if (properties.getMaxWait() != null) {
+            config.setMaxWaitMillis(properties.getMaxWait().toMillis());
+        }
+        return config;
+    }
+
+    @Bean
+    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
+
+    @Bean
+    public ValueOperations<String, String> valueOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForValue();
+    }
+
+    @Bean
+    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
+
+    @Bean
+    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForSet();
+    }
+
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
+
+}
---
+++ src/main/java/com/example/springcloud/gatewaysimple/route/RedisRouteDefinitionRepository.java
@@ -0,0 +1,55 @@
+package com.example.springcloud.gatewaysimple.route;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public class RedisRouteDefinitionRepository implements RouteDefinitionRepository {
+
+    private static final String GATEWAY_ROUTE ="gateway_dynamic_route";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+
+        List<RouteDefinition> routeDefinitionList = Lists.newArrayList();
+        redisTemplate.opsForHash().values(GATEWAY_ROUTE).stream().forEach(route->{
+            RouteDefinition routeDefinition = JSON.parseObject(route.toString(), RouteDefinition.class);
+            routeDefinitionList.add(routeDefinition);
+        });
+        return Flux.fromIterable(routeDefinitionList);
+    }
+
+    @Override
+    public Mono<Void> save(Mono<RouteDefinition> route) {
+
+        return route.flatMap(routeDefinition -> {
+            redisTemplate.opsForHash().put(GATEWAY_ROUTE,routeDefinition.getId(),JSON.toJSON(routeDefinition));
+            return Mono.empty();
+        });
+    }
+
+    @Override
+    public Mono<Void> delete(Mono<String> routeId) {
+
+        return routeId.flatMap(id->{
+            if(redisTemplate.opsForHash().hasKey(GATEWAY_ROUTE,id)){
+                redisTemplate.opsForHash().delete(GATEWAY_ROUTE,id);
+                return Mono.empty();
+            }else {
+                return Mono.defer(()->Mono.error(new Exception("routeDefinition not found :"+routeId)));
+            }
+        });
+    }
+}
